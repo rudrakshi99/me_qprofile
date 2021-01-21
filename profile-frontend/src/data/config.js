d2e9a2bb903765848d3b6dd725e99ce2e7608e8e
rudrakshi99 <sonirudrakshi99@gmail.com>
2021-01-21 17:52:21 +0530

    initial commit

---
+++ profile-frontend/src/data/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    // TODO: change your profile information here
+    name: "Rudrakshi",
+    greeting: "Hey 👋",
+    greetingDescription: "I'm Rudrakshi and I'm a Software Engineer!",
+    githubUrl: "https://github.com/rudrakshi99",
+    linkedinUrl: "https://www.linkedin.com/in/rudrakshi-soni-403031195/",
+    cvLink: "https://drive.google.com/file/d/18jCFPRvZPV0Rwk6p-27_GWqDwF0fFZPo/view?usp=sharing",
+};
